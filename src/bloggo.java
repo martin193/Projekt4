20b34fa8fca6b382921b7b0b5841a52f98d792ad
matildawe <63040887+matildawe@users.noreply.github.com>
2020-04-03 12:30:37 +0200

    shjjs

--- src/bloggo.java
+++ src/bloggo.java
@@ -13,6 +13,6 @@ public class bloggo {
     {
         System.out.println("hej");
         System.out.println("ohej");
-        System.out.println("aja");
+        System.out.println("bbhjjbhh");
     }
 }
