08a58e68dfd5346a6c44d0bee3b2baf4d518715b
martin193 <50623887+martin193@users.noreply.github.com>
2020-04-14 11:03:41 +0200

    Update testardatabas.java

--- src/testardatabas.java
+++ src/testardatabas.java
@@ -6,16 +6,18 @@ import java.sql.SQLException;
 
 public class testardatabas {
     
-        public void Check_Data(){
+        public String Anvandare(){
+            String namn = null;
 	try {
             String query="select * from Anvandare";
             Connection con = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/team4databas","marcusbatan","MsJvyxCm#4JQz8B");
             PreparedStatement ps= con.prepareStatement(query);
 	    ResultSet rs=ps.executeQuery();
-	        System.out.println(rs.getString("Anvandare"));
+	    namn = rs.getString(1);
 	} catch (SQLException e) {
 	    System.out.println(e);
 	}
+        return namn;
     }
 }
 
