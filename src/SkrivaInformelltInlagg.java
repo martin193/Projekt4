6a181e121f67789fef5f5737c223e33b9d39c021
Bendrik <oskar.bendrik@gmail.com>
2020-04-15 13:24:42 +0200

    inf+nytt id fullösning

--- src/SkrivaInformelltInlagg.java
+++ src/SkrivaInformelltInlagg.java
@@ -161,13 +161,14 @@ public class SkrivaInformelltInlagg extends javax.swing.JFrame {
     private void btnPostaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPostaActionPerformed
         try
                 {
-                //String fraga = ("INFORMELL_BLOGG", "INLAGGSID");
+           
+                String fraga = ("SELECT INLAGGSID FROM INFORMELL_BLOGG");
                 
                         
-                GetQuery(fraga);
+                int grej = GetQuery(fraga);
    
                 
-                
+                //"select * from FORMELL_BLOGG JOIN KATEGORI_FORMELL ON KATEGORIID = KATEGORI where ANVANDARID = (select ANVANDARID from ANVANDARE where EPOST = '" + epost + "')";
                 
                 }
                 catch (Exception bla)
@@ -176,31 +177,28 @@ public class SkrivaInformelltInlagg extends javax.swing.JFrame {
                 }   
     }//GEN-LAST:event_btnPostaActionPerformed
 
-    public String GetQuery(String s)
+    public int GetQuery(String s)
     {
 	DB_connection.DB_Connection obj_DB_Connection= new DB_connection.DB_Connection();
 	Connection connection=obj_DB_Connection.get_connection();
 	PreparedStatement ps=null;
-        String idNy = null;
+        int idNy = 0;
 
 	try {
 	    String query= s;
 	    ps=connection.prepareStatement(query);
 	    ResultSet rs=ps.executeQuery();
 	    while(rs.next()){
-                idNy = rs.getAutoIncrement;
-
-                
-                
-                
-                JOptionPane.showMessageDialog(null, " nästa id: " + idNy);
-                
+                idNy = rs.getInt(1);
+            
                 
 	    }
 	} catch (Exception e) {
 	    System.out.println(e);
 	} 
-        return rubrik;
+        idNy = idNy + 1;
+        JOptionPane.showMessageDialog(null, " nästa id: " + idNy);
+        return idNy;
     }
 
 
