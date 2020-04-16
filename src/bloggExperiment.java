5b8f9f5a83e027fee66fd6d7ac3232e2f466a6c6
Bendrik <oskar.bendrik@gmail.com>
2020-04-16 14:12:49 +0200

    dunno

--- src/bloggExperiment.java
+++ src/bloggExperiment.java
@@ -121,19 +121,31 @@ public class bloggExperiment extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         lblBild = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        txtBlogg = new javax.swing.JTextArea();
+        bloggRuta = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         tblInlagg.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null}
+
             },
             new String [] {
                 "Rubrik", "Författare", "Kategori"
             }
         ));
+        tblInlagg.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblInlaggMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tblInlagg);
+
+        cbxKategorier.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbxKategorierActionPerformed(evt);
+            }
+        });
 
         btnFiltreraKategori.setText("kategori");
         btnFiltreraKategori.addActionListener(new java.awt.event.ActionListener() {
@@ -146,9 +158,11 @@ public class bloggExperiment extends javax.swing.JFrame {
 
         lblBild.setPreferredSize(new java.awt.Dimension(208, 212));
 
-        txtBlogg.setColumns(20);
-        txtBlogg.setRows(5);
-        jScrollPane2.setViewportView(txtBlogg);
+        bloggRuta.setColumns(20);
+        bloggRuta.setRows(5);
+        jScrollPane2.setViewportView(bloggRuta);
+
+        jLabel2.setText("hej där tryck på ett inlägg");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -167,7 +181,9 @@ public class bloggExperiment extends javax.swing.JFrame {
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(btnFiltreraKategori, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                     .addComponent(cbxKategorier, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 192, Short.MAX_VALUE)))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+                                .addComponent(jLabel2)
+                                .addGap(0, 25, Short.MAX_VALUE)))
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 377, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(60, 60, 60)
@@ -186,7 +202,9 @@ public class bloggExperiment extends javax.swing.JFrame {
                         .addGap(39, 39, 39)
                         .addComponent(btnFiltreraKategori)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(cbxKategorier, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(cbxKategorier, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(21, 21, 21)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -208,7 +226,7 @@ public class bloggExperiment extends javax.swing.JFrame {
         try
         {
             
-            String fraga= "SELECT * FROM INFORMELL_BLOGG JOIN KATEGORI_INFORMELL ON KATEGORIID = KATEGORI WHERE KATEGORINAMN = '" + kategori + "'";
+            String fraga= "SELECT * FROM INFORMELL_BLOGG JOIN KATEGORI_INFORMELL ON KATEGORIID = KATEGORI JOIN ANVANDARE ON ANVANDARE.ANVANDARID = INFORMELL_BLOGG.ANVANDARID WHERE KATEGORINAMN = '" + kategori + "'";
                 
                 
             String rubrik = GetQuery(fraga);
@@ -221,6 +239,22 @@ public class bloggExperiment extends javax.swing.JFrame {
         }
          
     }//GEN-LAST:event_btnFiltreraKategoriActionPerformed
+
+    private void cbxKategorierActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbxKategorierActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cbxKategorierActionPerformed
+
+    private void tblInlaggMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblInlaggMouseClicked
+        
+        JOptionPane.showMessageDialog(null, "kewl");
+        
+        getSelectedRow();
+
+        bloggRuta.setText("");
+
+        
+        
+    }//GEN-LAST:event_tblInlaggMouseClicked
 
     /**
      * @param args the command line arguments
@@ -258,13 +292,14 @@ public class bloggExperiment extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea bloggRuta;
     private javax.swing.JButton btnFiltreraKategori;
     private javax.swing.JComboBox<String> cbxKategorier;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel lblBild;
     private javax.swing.JTable tblInlagg;
-    private javax.swing.JTextArea txtBlogg;
     // End of variables declaration//GEN-END:variables
 }
